50c4fb83f2c9fd26f1f5c544dd3911cc3f342263
pathum-kalhan <dev1@eatmeglobal.com>
2020-03-04 13:29:54 +0530

    add few bad words

--- src/badWords.js
+++ src/badWords.js
@@ -1,5 +1,8 @@
 const badWords = [
   'fuck', 'bitch',
+  'huththo', 'huththi', 'huththiye',
+  'hutto', 'hutti', 'huttiye',
+  'pakaya', 'wesi', 'wesiye', 'keri', 'keriya',
 ];
 
 module.exports = badWords;
